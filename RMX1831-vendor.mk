90c4055e1a44dd2d3cff71354c53501359d2fa32
TheMalachite <eliasgheeraert@gmail.com>
2021-01-08 19:14:34 +0530

    RMX1831-vendor: add system side TEE and Soter blobs
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- RMX1831-vendor.mk
+++ RMX1831-vendor.mk
@@ -7,4 +7,21 @@ PRODUCT_COPY_FILES += \
     vendor/realme/RMX1831/proprietary/lib/libshowlogo.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libshowlogo.so \
     vendor/realme/RMX1831/proprietary/lib/libsysenv_system.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libsysenv_system.so \
     vendor/realme/RMX1831/proprietary/lib64/libmtk-ril.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libmtk-ril.so \
-    vendor/realme/RMX1831/proprietary/lib64/libsysenv_system.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libsysenv_system.so 
+    vendor/realme/RMX1831/proprietary/lib64/libsysenv_system.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libsysenv_system.so \
+    vendor/realme/RMX1831/proprietary/lib/libMcRegistry.so:system/lib/libMcRegistry.so \
+    vendor/realme/RMX1831/proprietary/lib/libTeeClient.so:system/lib/libTeeClient.so \
+    vendor/realme/RMX1831/proprietary/lib/libTeeServiceJni.so:system/lib/libTeeServiceJni.so \
+    vendor/realme/RMX1831/proprietary/lib/libteeservice_client.trustonic.so:system/lib/libteeservice_client.trustonic.so \
+    vendor/realme/RMX1831/proprietary/lib/vendor.trustonic.tee@1.0.so:system/lib/vendor.trustonic.tee@1.0.so \
+    vendor/realme/RMX1831/proprietary/lib/vendor.trustonic.teeregistry@1.0.so:system/lib/vendor.trustonic.teeregistry@1.0.so \
+    vendor/realme/RMX1831/proprietary/lib64/libMcRegistry.so:system/lib64/libMcRegistry.so \
+    vendor/realme/RMX1831/proprietary/lib64/libTeeClient.so:system/lib64/libTeeClient.so \
+    vendor/realme/RMX1831/proprietary/lib64/libTeeServiceJni.so:system/lib64/libTeeServiceJni.so \
+    vendor/realme/RMX1831/proprietary/lib64/libteeservice_client.trustonic.so:system/lib64/libteeservice_client.trustonic.so \
+    vendor/realme/RMX1831/proprietary/lib64/vendor.trustonic.tee@1.0.so:system/lib64/vendor.trustonic.tee@1.0.so \
+    vendor/realme/RMX1831/proprietary/lib64/vendor.trustonic.teeregistry@1.0.so:system/lib64/vendor.trustonic.teeregistry@1.0.so \
+    vendor/realme/RMX1831/proprietary/priv-app/TeeService/lib/arm64/libTeeServiceJni.so:system/priv-app/TeeService/lib/arm64/libTeeServiceJni.so
+
+PRODUCT_PACKAGES += \
+    SoterService \
+    TeeService
